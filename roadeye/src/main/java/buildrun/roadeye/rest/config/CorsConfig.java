fad9bdf176a3c0c77d40a35d91ad45f9d332ba6c
Douglas Andrade Severa <douglas.severa@eximioti.com.br>
2024-04-02 10:13:12 -0300

    commit CorsConfig

--- roadeye/src/main/java/buildrun/roadeye/rest/config/CorsConfig.java
+++ roadeye/src/main/java/buildrun/roadeye/rest/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")//.allowedOrigins("http://dominio-permitido.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
